4971e436af8f8accdee37f73e46c5ad4c48452d1
Mr. Phil <mr.phil1@gmx.net>
2021-10-31 11:44:26 +0100

    add Tabbar.dart

---
+++ lib/Tabbar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TabBarDemo extends StatelessWidget {
+  const TabBarDemo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
