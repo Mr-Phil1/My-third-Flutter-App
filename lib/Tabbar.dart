9b8ed93ce62b1971acf1398ab1f1749f5d1c369d
Mr. Phil <mr.phil1@gmx.net>
2021-10-31 11:59:47 +0100

    modify Tabbar.dart * add tabbar

--- lib/Tabbar.dart
+++ lib/Tabbar.dart
@@ -5,6 +5,40 @@ class TabBarDemo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final tabs = [
+      "Alle",
+      "Freunde",
+      "Familie",
+      "Arbeit",
+    ];
+    final list = List<String>.generate(1337, (index) => "$index");
+
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("My Ultimate Chat"),
+          bottom: TabBar(
+            isScrollable: true,
+            tabs: [
+              for (final t in tabs)
+                Tab(
+                  text: t,
+                )
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            for (final t in tabs)
+              Center(child: ListView.builder(itemBuilder: (context, i) {
+                return ListTile(
+                  title: Text("$t - ${list[i]}"),
+                );
+              })),
+          ],
+        ),
+      ),
+    );
   }
 }
