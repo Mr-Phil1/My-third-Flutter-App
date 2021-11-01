d45f4a42499b41d1fd1f7434a16498c3b4ec9201
Mr. Phil <mr.phil1@gmx.net>
2021-11-01 18:39:50 +0100

    modify Tabbar.dart * add BottomAppBar

--- lib/Tabbar.dart
+++ lib/Tabbar.dart
@@ -38,6 +38,40 @@ class TabBarDemo extends StatelessWidget {
               })),
           ],
         ),
+        bottomNavigationBar: _MyAppBar(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => print("FAB"),
+          child: Icon(Icons.add),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      ),
+    );
+  }
+}
+
+class _MyAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      color: Theme.of(context).colorScheme.secondary,
+      shape: const CircularNotchedRectangle(),
+      child: IconTheme(
+        data: IconThemeData(
+          color: Theme.of(context).colorScheme.onPrimary,
+        ),
+        child: Row(
+          children: [
+            IconButton(
+                tooltip: "Menü öffnen",
+                icon: const Icon(Icons.menu),
+                onPressed: () => print("Open Menu Here")),
+            Spacer(),
+            IconButton(
+                tooltip: "Suche",
+                icon: const Icon(Icons.search),
+                onPressed: () => print("Open Search Here")),
+          ],
+        ),
       ),
     );
   }
